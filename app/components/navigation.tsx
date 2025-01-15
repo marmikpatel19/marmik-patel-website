6116b917d43c3a5bf08a9badeace4e2c23a6f2c1
Marmik Patel <marmikp19@gmail.com>
2025-01-15 14:08:34 -0600

    style nav

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -1,13 +1,20 @@
 'use client';
+import Link from "next/link";
 
 export default function Navigation(){
     return (
-        <div className="flex justify-between">
-            <div>Marmik Patel</div>
+        <div className="flex justify-between mt-6">
+            <div className="font-semibold">Marmik Patel</div>
             <div className="flex space-x-6">
-                <a className="underline">Engineer</a>
-                <a className="underline">Founder</a>
-                <a className="underline">Connect</a>
+                <Link className="underline font-semibold" href="/engineer">
+                    Engineer
+                </Link>
+                <Link className="underline font-semibold" href="/founder">
+                    Founder
+                </Link>
+                <Link className="underline font-semibold" href="/connect">
+                    Connect
+                </Link>
             </div>
         </div>
     );
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Navigation from "./components/navigation";
 
 export default function Home() {
   return (
-    <div className="max-w-screen-lg mx-auto">
+    <div className="max-w-custom mx-auto pl-10 pr-10">
       <Navigation />
     </div>
   );
