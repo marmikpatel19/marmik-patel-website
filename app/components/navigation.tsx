4fe4831b6467b62da3bc3579303bfd2b6a7f0ae1
Marmik Patel <marmikp19@gmail.com>
2025-01-16 00:56:45 -0600

    link title

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -4,7 +4,11 @@ import Link from "next/link";
 export default function Navigation(){
     return (
         <div className="flex flex-col sm:flex-row justify-between mt-6">
-            <div className="font-semibold">Marmik Patel</div>
+            <div className="font-semibold">
+                <Link href="/">
+                Marmik Patel
+                </Link>
+            </div>
             <div className="flex space-x-6 mt-4 sm:mt-0">
                 <Link className="underline font-semibold" href="/engineer">
                     Engineer
